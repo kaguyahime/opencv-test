ba4b2d37f7b10f2a578d3b4914c6ea33dc8766d0
kaguyahime <834548154@qq.com>
2020-10-15 17:07:41 +0800

    更新第5天的小练习

---
+++ day5/medium.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/15 16:44
+# @Author  : panfei
+# @FileName: medium.py
+# @Software: PyCharm
+# @Cnblogs ：https://github.com/kaguyahime
+
+import numpy as np
+import cv2
+
+def medium_filter(img,k_size=3):
+    W,H,C = img.shape
+
+    #进行补零操作
+    pad = k_size//2
+    out = np.zeros((H+pad*2,W+pad*2,C),dtype=np.float)
+    out[pad:pad+H,pad:pad+W]=img.copy().astype(np.float)
+
+    tmp = out.copy()
+    #用中值替换原来的值
+    for y in range(H):
+        for x in range(W):
+            for c in range(C):
+                out[pad+y,pad+x,c]=np.median(tmp[y:y+k_size,x:x+k_size,c])
+
+    out = out.astype(np.uint8)
+
+    return out
+
+if __name__ == '__main__':
+    img = cv2.imread('imori_noise.jpg')
+    out = medium_filter(img)
+    cv2.imshow('out',out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
