5f14e97a761e59bc783df61efc8140ebc17e1216
panfei <834548154@qq.com>
2020-11-08 15:20:50 +0800

    增加day8直方图操作，修改直方图均值和标准差

---
+++ day8/histequalization.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/11/8 14:22
+# @Author  : panfei
+# @FileName: histequalization.py
+# @Software: PyCharm
+# @Cnblogs ：https://github.com/kaguyahime
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def equal(img,m0=128,s0=52):
+    m = np.mean(img)
+    print(m)
+    s = np.std(img)
+    print(s)
+
+    out = img.copy()
+
+    out = s0/s*(out-m)+m0
+    out[out<0]=0
+    out[out>255]=255
+
+    print(np.mean(out))
+    print(np.std(out))
+
+    out = out.astype(np.uint8)
+
+    return out
+
+
+def hist_normalization(img,a = 0,b = 255):
+    #将图形中原来的只扩展到（0-255）之间使值的分布更加宽，对比更加明显
+    c = img.min()
+    d = img.max()
+
+    out = img.copy()
+
+    out = (b-a)/(d-c)*(out-c)+a
+
+    out[out<a]=a
+    out[out>b]=b
+
+    out = out.astype(np.uint8)
+    return out
+
+
+
+
+
+if __name__ == '__main__':
+    img = cv2.imread('imori_dark.jpg').astype(np.float)
+    out = equal(img)
+    out1 = hist_normalization(img)
+    plt.hist(out.ravel(),bins=255,rwidth=0.8,range=(0,255))
+    plt.hist(out1.ravel(),bins=255,rwidth=1,range=(0,255))
+    plt.hist(img.ravel(), bins=255, rwidth=1, range=(0, 255))
+    plt.show()
+    cv2.imshow('out', out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
