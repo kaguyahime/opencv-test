f6219149faf7a33e2fc0b2748ea0ed15fbd3b6b0
kaguyahime <834548154@qq.com>
2020-12-08 17:44:28 +0800

    修改day9中，不能扩大两倍的问题，四舍五入会超过原图的索引

--- day5/medium.py
+++ day5/medium.py
@@ -13,13 +13,13 @@ def medium_filter(img,k_size=3):
 
     #进行补零操作
     pad = k_size//2
-    out = np.zeros((H+pad*2,W+pad*2,C),dtype=np.float)
-    out[pad:pad+H,pad:pad+W]=img.copy().astype(np.float)
+    out = np.zeros((W+pad*2,H+pad*2,C),dtype=np.float)
+    out[pad:pad+W,pad:pad+H]=img.copy().astype(np.float)
 
     tmp = out.copy()
     #用中值替换原来的值
-    for y in range(H):
-        for x in range(W):
+    for y in range(W):
+        for x in range(H):
             for c in range(C):
                 out[pad+y,pad+x,c]=np.median(tmp[y:y+k_size,x:x+k_size,c])
 
@@ -28,7 +28,7 @@ def medium_filter(img,k_size=3):
     return out
 
 if __name__ == '__main__':
-    img = cv2.imread('imori_noise.jpg')
+    img = cv2.imread('123.jpg')
     out = medium_filter(img)
     cv2.imshow('out',out)
     cv2.waitKey(0)
--- day9/nn_interpolate.py
+++ day9/nn_interpolate.py
@@ -15,15 +15,60 @@ def interpolate(img,aw,ah):
 
     y = np.arange(aH).repeat(aW).reshape(aW,-1)
     x = np.tile(np.arange(aW),(aH, 1))
-    y = np.round(y / aH).astype(np.int)
-    x = np.round(x / aW).astype(np.int)
+    y = (y / ah).astype(np.int)
+    x = (x / aw).astype(np.int)
 
-    out = img[y, x]
+    #[0,0,0]，[0,1,1]一维内加一个代表行内加一个，其中第一个数组中代表原图的行，第二个数组代表原图中的列
+    #
+    out = img[y,x].astype(np.uint8)
+    out1 = img[y,x].astype(np.uint8)
+    print(out)
+    return out
 
 if __name__ == '__main__':
 
-    img = cv2.imread('imori.jpg')
-    interpolate(img,1,1)
+    img = cv2.imread('imori.jpg').astype(np.float)
+    out = interpolate(img,3,3)
+    cv2.imshow('out',out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+#
+# # Nereset Neighbor interpolation
+# def nn_interpolate(img, ax=1, ay=1):
+#     H, W, C = img.shape
+#
+#     aH = int(ay * H)
+#     aW = int(ax * W)
+#
+#     y = np.arange(aH).repeat(aW).reshape(aW, -1)
+#     x = np.tile(np.arange(aW), (aH, 1))
+#     y = np.round(y / ay).astype(np.int)
+#     x = np.round(x / ax).astype(np.int)
+#
+#     out = img[y,x]
+#
+#     out = out.astype(np.uint8)
+#
+#     return out
+#
+#
+# # Read image
+# img = cv2.imread("imori.jpg").astype(np.float)
+#
+# # Nearest Neighbor
+# out = nn_interpolate(img, ax=1.5, ay=1.5)
+#
+# # Save result
+# cv2.imshow("result", out)
+# cv2.waitKey(0)
+# cv2.imwrite("out.jpg", out)
+
+
 
 
 
